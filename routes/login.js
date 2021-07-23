740e19750a240606f0f5f6531f8eb34d77b1fa93
Hernan Condori <hcondori@elementum.com>
2021-07-22 22:32:25 -0300

    Mongoose lean metodo

--- routes/login.js
+++ routes/login.js
@@ -9,31 +9,33 @@ router.post('/', async (req, res)  => {
   const user = await UserModel.findOne({
     email: req.body.email,
     password: req.body.password
-  });
+  }).lean();
+
   if (user) {
-    const {
-      _id,
-      email,
-      firstName,
-      lastName,
-    } = user;
+    // const {
+    //   _id,
+    //   email,
+    //   firstName,
+    //   lastName,
+    // } = user;
     // ES IGUAL A 
     // const _id = user._id;
     // const email = user.email;
     // const firstName = user.firstName;
     // const lastName = user.lastName;
 
-    const usePayload = {
-      _id,
-      email,
-      firstName,
-      lastName,
-    }
-
-    var token = jwt.sign(usePayload, 'shhhhh');
+    // const usePayload = {
+    //   _id,
+    //   email,
+    //   firstName,
+    //   lastName,
+    // }
+    delete user.password;
     
-    console.log(token);
-    res.send({ token: token, user: usePayload });
+    var token = jwt.sign(user, 'shhhhh');
+    
+    console.log('token: ', token);
+    res.send({ token: token, user: user });
     
   } else { // IMPORTANTE SINO SE EJECUTAN LOS @ SEND
     res.status(400).send({ error: 'usuario no encontrado' });
